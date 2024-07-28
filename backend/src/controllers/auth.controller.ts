1cabd38236c79c568b387b3cf608c63e86bb72da
Praveen Goswami <praveen828589@gmail.com>
2024-07-28 18:13:48 +0530

    create: signin api route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/errorHandler";
+import jwt from "jsonwebtoken";
 
 export const signupController = async (
   req: Request,
@@ -16,7 +17,7 @@ export const signupController = async (
     })
 
     if(existingUser) {
-        next(errorHandler(400, "Username or email already exists"));
+      next(errorHandler(400, "Username or email already exists"));
     }
 
     //Hashing the password for security
@@ -33,3 +34,52 @@ export const signupController = async (
     next(error);
   }
 };
+
+export const signinController = async (req:Request, res:Response, next: NextFunction) => {
+  const { identifier, password } = req.body;
+
+  try {
+    //Finding if the user is valid
+    const validUser = await User.findOne({
+      $or: [{ username: identifier }, { email: identifier }]
+    })
+
+    if(!validUser) {
+      next(errorHandler(404, "User not found"));
+    } else {
+      //Checking if password matches
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+      if(!validPassword) {
+        next(errorHandler(401, "Invalid login credentials"));
+      }
+
+      //Create and sign JWT token
+      const token = jwt.sign(
+        { userId: validUser._id },
+        process.env.JWT_SECRET as string,
+        { expiresIn: '24h' }
+      )
+
+      // Set token in cookie
+      res.cookie('access_token', token, {
+        // Helps prevent XSS attacks by restricting access to the cookie
+        httpOnly: true, 
+        // Ensures the cookie is only sent over HTTPS in production
+        // secure: process.env.NODE_ENV === 'production',
+        // 24 hours 
+        maxAge: 24 * 60 * 60 * 1000, 
+      });
+
+      const { _id, username, email } = validUser;
+
+      res.status(200).json({
+        success: true,
+        message: "Login successful",
+        user: { _id, username, email }
+      });
+    }
+
+  } catch (error) {
+    next(error);
+  }
+}
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signupController } from "../controllers/auth.controller";
+import { signinController, signupController } from "../controllers/auth.controller";
 
 //Creating an express router instance
 const router = Router();
 
 router.post("/signup", signupController);
+router.post("/signin", signinController);
 
 export default router;
